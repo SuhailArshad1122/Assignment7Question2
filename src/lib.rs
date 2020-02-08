6750ca730ee40bec3e5b57c63a7443cfdeed6560
Suhail Arshad <suhail_a@hotmail.com>
2020-02-09 02:16:28 +0500

    Assignment7Question2

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod mod_in_lib1 {
+    pub mod mod_in_lib2 {
+        pub fn simple_fn() {
+            println!("Simple Function in sub module from lib");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod lib;
+fn main () {
+    lib::mod_in_lib1::mod_in_lib2::simple_fn()
+}
